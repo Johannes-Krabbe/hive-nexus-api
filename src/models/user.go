f54b8fb7d3c7f76dc6181d30141a4189ab9e59da
Nima Azarbayjani <nima.azarbayjani@digitalturbine.com>
2023-02-18 22:37:52 +0100

    Add elementary check for username

--- src/models/user.go
+++ src/models/user.go
@@ -4,6 +4,8 @@ import (
 	"time"
 
 	"gorm.io/gorm"
+
+	valid "github.com/asaskevich/govalidator"
 )
 
 type User struct {
@@ -12,8 +14,23 @@ type User struct {
 	UpdatedAt time.Time
 	DeletedAt gorm.DeletedAt
 
-    Username string `json:"Username" gorm:"type: varchar(32) not null unique"`
-    Email string `json:"Email" gorm:"type: varchar(128) not null unique"`
+	Username string `json:"Username" gorm:"type: varchar(32) not null unique valid:"usernameValidator, length(4|32)"`
+	Email    string `json:"Email" gorm:"type: varchar(128) not null unique" valid:"email"`
 	Password string `json:"Password" gorm:"type: varchar(128) not null"`
-	Salt string `json:"Salt" gorm:"type: varchar(128) not null"`
+	Salt     string `json:"Salt" gorm:"type: varchar(128) not null"`
+}
+
+func userModelInit() {
+	valid.CustomTypeTagMap.Set("usernameValidator", func(i interface{}, context interface{}) bool {
+		switch v := context.(type) {
+		case User:
+			for _, c := range v.Username {
+				if valid.IsNumeric(string(c)) || valid.IsLowerCase(string(c)) || string(c) == "-" || string(c) == "_" {
+					return false
+				}
+			}
+			return true
+		}
+		return false
+	})
 }
