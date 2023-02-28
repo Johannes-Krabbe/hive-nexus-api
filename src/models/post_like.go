301cb7f1e570453b48900324c56915ff6ec8737b
Nima Azarbayjani <nima.azarbayjani@digitalturbine.com>
2023-02-28 12:27:57 +0100

    Remove DeletedAt field from PostLike model to avoid soft delete by gorm

--- src/models/post_like.go
+++ src/models/post_like.go
@@ -4,14 +4,12 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"gorm.io/gorm"
 )
 
 type PostLike struct {
 	ID        uuid.UUID `gorm:"primaryKey;type:uuid;default:gen_random_uuid()"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
-	DeleteAt  gorm.DeletedAt
 
 	UserID uuid.UUID `json:"UserID"`
 	PostID uuid.UUID `json:"PostID"`
